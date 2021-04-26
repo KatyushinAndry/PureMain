35a1bd37fd615682487c6b98e0f7857adf7d426d
My Name <belkatan@yandex.ru>
2021-04-26 22:07:31 +0300

    адд project

---
+++ src/test/java/pure/autotests/pages/MainPages.java
@@ -0,0 +1,17 @@
+package pure.autotests.pages;
+
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPages {
+
+    public MainPages opensHeader(){
+        $(".announcements__read-more").scrollTo();
+        $("#navbar").shouldBe(visible);
+        return this;
+    }
+
+
+}
